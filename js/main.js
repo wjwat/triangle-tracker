4718f440427eb3b0f7c384490befb14bcd101f8a
William Watkins <wjwat@onionslice.org>
2022-01-13 17:28:06 -0600

    Finalize functionality of triangle checker
    Co-authored-by: Liam Eller <liamthelastson@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
+	$('button#clear').click(function () {
+		$('h2#output').text('');
+	});
+
 	$('form').submit(function(e) {
 		e.preventDefault();
+		a = parseInt($('input#side-a').val());
+		b = parseInt($('input#side-b').val());
+		c = parseInt($('input#side-c').val());
+
+		// only positive numbers
+		if (a <= 0 || b <= 0 || b <= 0) {
+			$('h2#output').text('This triangle doesn\'t exist!');
+		}	else if (a >= (b + c) || b >= (a + c) || c >= (a + b)) {
+			$('h2#output').text('Not a triangle');
+		} else if (a === b && b === b) {
+			$('h2#output').text('Equilateral');
+		} else if (a !== b && b !== b && b !== a) {
+			$('h2#output').text('Scalene');
+		} else {
+			$('h2#output').text('Isosceles');
+		}
 	});
 });
